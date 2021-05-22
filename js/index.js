ca7c148e62be59c7d0814d989a87d31d99812a3d
Shayan Karimi <shayankarimi0078@gmail.com>
2021-05-22 23:02:07 +0430

    Post

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const postsDOM = document.getElementById('posts')
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(data => {
+    loadPosts(data)
+  })
+  .catch(error => console.error(error))
+
+
+const loadPosts = (posts) => {
+  posts.forEach(post => {
+    const postChild = createPost(post)
+
+    postsDOM.appendChild(postChild)
+  })
+}
+
+const createPost = ({ title, body }) => {
+  const post = document.createElement('post-component')
+
+  post.setAttribute('title', title)
+  post.setAttribute('text', body)
+
+  return post
+}
