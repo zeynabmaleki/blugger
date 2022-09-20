e27d5253d8464d0d01cc8bac3cb6d420a35b9dd2
zizimrad <73473371+zeynabmaleki@users.noreply.github.com>
2022-09-20 19:44:31 +0430

    Update zizi-album.js

--- js/zizi-album.js
+++ js/zizi-album.js
@@ -1,1 +1,4 @@
-console.log('hello')
+fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
