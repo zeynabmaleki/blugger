d2b87a104851b7fa1795e05ea1fae651b3a9616e
zizimrad <73473371+zeynabmaleki@users.noreply.github.com>
2022-09-21 13:50:08 +0430

    Update zizi-album.js

--- js/zizi-album.js
+++ js/zizi-album.js
@@ -1,4 +1,38 @@
+const albumDOM = document.getElementById('album')
+
 fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    LoadAlbum(data)
+  })
   .catch(error => console.error(error))
+
+
+const LoadAlbum = (album) => {
+  album.forEach(photo => {
+    const photochild = createphoto(photo)
+
+    albumDOM.appendChild(photochild)
+  })
+}
+
+const createphoto = ({
+  url,
+  title
+}) => {
+  const photo = document.createElement('div')
+
+  photo.classList.add('col')
+  photo.classList.add('col-md-4')
+  photo.classList.add('mt-4')
+
+  photo.innerHTML = `
+  <div class="card">
+    <img src="${url}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+    </div>
+  </div>
+  `
+  return photo
+}
